64293c53572f2756b0dd86359d69e1ae7d5d39b9
salva <salva@DESKTOP-UOJP10O>
2022-03-07 19:21:21 +0100

    refactorización realizada

--- Vivienda/src/Vivienda.java
+++ Vivienda/src/Vivienda.java
@@ -10,20 +10,86 @@
  */
 public class Vivienda 
 {
-    double precio;
-    int numHabitaciones;
-    double superficie;
-    boolean parking;
-    String estado;
-    String propietario;
+        private double precio;
+        private int numHabitaciones;
+        private double superficie;
+        private boolean parking;
+        private String estado;
+        private String propietario;
+        
+        public double getPrecio() {
+            return precio;
+        }
+        
+        public void setPrecio(double precio) {
+            this.precio = precio;
+        }
+        
+        public int getNumHabitaciones() {
+            return numHabitaciones;
+        }
+        
+        public void setNumHabitaciones(int num) {
+            this.numHabitaciones = num;
+        }
+
+        public double getSuperficie() {
+            return superficie;
+        }
+        
+        public void setSuperficie(double superficie) {
+            this.superficie = superficie;
+        }
+        
+        public boolean getParking() {
+            return parking;
+        }
+        
+        public void setParking(boolean hay) {
+            this.parking = hay;
+        }
+        
+        public String getEstado(){
+            return estado;
+        }
+
+        public void setEstado(String estado) {
+            this.estado = estado;
+        }
+        
+        public String getPropietario() {
+            return propietario;
+        }
+        
+        public void setPropietario(String propietario) {
+            this.propietario = propietario;
+        }
     
     public Vivienda (double precio)
     {
         this.precio = precio;
     }
     
-    public void actualizarPrecio()
+    public Vivienda( double precio, int numHabitaciones, double superficie, boolean parking, String estado, String propietario) {
+        this.precio = precio;
+        this.numHabitaciones = numHabitaciones;
+        this.superficie = superficie;
+        this.parking = parking;
+        this.estado = estado;
+        this.propietario = propietario;   
+    }   
+    
+    public void actualizarPrecio(double descuento)
     {
-        precio = precio - precio * 0.05;
+        precio -= precio * descuento;
+    }
+    
+    public static void imprimirVivienda(Vivienda casa1) {
+        System.out.println("Precio: " + casa1.getPrecio());
+        System.out.println("Número de habitaciones: "+ casa1.getNumHabitaciones());
+        System.out.println("Superficie en m2: " + casa1.getSuperficie());
+        System.out.println("¿Dispone de parking?: " + casa1.getParking());
+        System.out.println("Estado: "+ casa1.getEstado());
+        System.out.println("Propietario: "+ casa1.getPropietario());
     }
 }
